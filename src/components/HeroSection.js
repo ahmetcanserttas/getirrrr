4cb392649eca1200ed9cfd2e6989524692c2d5dd
ahmetcanserttas <119846981+ahmetcanserttas@users.noreply.github.com>
2023-02-22 09:18:46 +0300

    version 3 prod

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -16,13 +16,13 @@ export default function HeroSection() {
     };
 
   return (
-    <div className="">
+    <div className="relative before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
         <Slider {...settings}>
           <div>
-            <img className="w-full h-[500px] object-cover" src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-1.jpg" alt=""/>
+            <img className="w-full h-[500px] object-cover" src="https://fastly.picsum.photos/id/112/1366/500.jpg?hmac=1xbGCSoe9lXk_DCi2QCrb_MyAK_2l0Q8_DrZGGqtWSo" alt=""/>
           </div> 
           <div>
-            <img className="w-full h-[500px] object-cover" src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-2.jpg" alt=""/>
+            <img className="w-full h-[500px] object-cover" src="https://fastly.picsum.photos/id/292/1366/500.jpg?hmac=8_V7Y1qVghKhOaHiJrTwfR4O1W9aRgnBAfOzTpSpLls" alt=""/>
           </div>
           <div>
             <img className="w-full h-[500px] object-cover" src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-3.jpg" alt=""/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: theme =>({
-        'brand-color':'#5d3ebc'
+        'primary-brand-color':'#5d3ebc',
+        'secondary-brand-color':'#7849f7',
+        'brand-color':'#4c3398'
       })
     },
   },
