173968c7e082bf37745acde84fb79d941609df14
ahmetcanserttas <119846981+ahmetcanserttas@users.noreply.github.com>
2023-02-23 12:04:42 +0300

    gradient testing

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -16,19 +16,19 @@ export default function HeroSection() {
     };
 
   return (
-    <div className="relative before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
+    <div className="relative before:bg-gradient-to-r before:from-purple-500 before:to-transparent before:absolute before:inset-0">
         <Slider {...settings}>
           <div>
-            <img className="w-full h-[500px] object-cover" src="https://fastly.picsum.photos/id/112/1366/500.jpg?hmac=1xbGCSoe9lXk_DCi2QCrb_MyAK_2l0Q8_DrZGGqtWSo" alt=""/>
+            <img className="w-full h-[500px] object-cover" src="https://picsum.photos/id/13/1366/500" alt=""/>
           </div> 
           <div>
-            <img className="w-full h-[500px] object-cover" src="https://fastly.picsum.photos/id/292/1366/500.jpg?hmac=8_V7Y1qVghKhOaHiJrTwfR4O1W9aRgnBAfOzTpSpLls" alt=""/>
+            <img className="w-full h-[500px] object-cover" src="https://picsum.photos/id/14/1366/500" alt=""/>
           </div>
           <div>
-            <img className="w-full h-[500px] object-cover" src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-3.jpg" alt=""/>
+            <img className="w-full h-[500px] object-cover" src="https://picsum.photos/id/15/1366/500" alt=""/>
           </div>
           <div>
-            <img className="w-full h-[500px] object-cover" src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-4.jpg" alt=""/>
+            <img className="w-full h-[500px] object-cover" src="https://picsum.photos/id/16/1366/500" alt=""/>
           </div>
         </Slider>
     </div>  
