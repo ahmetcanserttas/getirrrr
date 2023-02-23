c7779bced267f3928a1fe25aabc0a3505c75d23c
ahmetcanserttas <119846981+ahmetcanserttas@users.noreply.github.com>
2023-02-23 12:04:27 +0300

    Gradient testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
         'primary-brand-color':'#5d3ebc',
         'secondary-brand-color':'#7849f7',
         'brand-color':'#4c3398'
+      }),
+      gradientColorStops: theme =>({
+        'primary-brand-color':'#5d3ebc',
+        'secondary-brand-color':'#7849f7',
+        'brand-color':'#4c3398'
       })
     },
   },
