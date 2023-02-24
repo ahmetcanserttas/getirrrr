43537e971ca47f65ec919ffa9f0843087161243d
ahmetcanserttas <119846981+ahmetcanserttas@users.noreply.github.com>
2023-02-24 11:17:58 +0300

    HeroSection-done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundColor: theme =>({
+      colors: {
         'primary-brand-color':'#5d3ebc',
         'secondary-brand-color':'#7849f7',
-        'brand-color':'#4c3398'
-      }),
-      gradientColorStops: theme =>({
-        'primary-brand-color':'#5d3ebc',
-        'secondary-brand-color':'#7849f7',
-        'brand-color':'#4c3398'
-      })
+        'brand-color':'#4c3398',
+        'brand-yellow': '#ffb400'
+      }
     },
   },
   plugins: [],
