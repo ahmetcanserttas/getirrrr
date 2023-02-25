b94fda49a15f67ae5f4d598d6c8db867b6b5c722
ahmetcanserttas <119846981+ahmetcanserttas@users.noreply.github.com>
2023-02-25 14:15:04 +0300

    Champaigns.finishes

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import categoryData from 'api/categories.json'
+import Category from 'components/ui/Category'
 
 export default function Categories() {
+  
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    
+    setTimeout(()=> setCategories(categoryData), 1000);
+
+  },[])
+
   return (
-    <div className=''>
+    <React.Fragment>
+    <div className='bg-white py-4'>
+      <div className='container mx-auto'>
+        <h3 className='text-sm font-semibold mb-3'>Kategoriler</h3>
+        <div className='grid grid-cols-10'>
+          {!categories.length && <div className='col-span-10'>Loading...</div>}
+          {categories && categories.map((category, index) => <Category key={index} category={category} />)}
+        </div>
+      </div>
     </div>
+    </React.Fragment>
   )
 }
---
+++ src/components/ui/Category.js
@@ -0,0 +1,14 @@
+ import React from "react";
+
+ export default function Category({category:{id, title, image}}) {
+
+    return(
+        <React.Fragment>
+            <a href="/" className="flex flex-col group items-center gap-y-2 text-center p-4 transition hover:bg-purple-50 rounded">
+                <img src={image} alt={title} className="w-12 h-12 rounded border-rounded border-gray-200"/>
+                <span className="text-sm font-semibold text-gray-700 group-hover:text-brand-color tracking-tight">{title}</span>
+            </a>
+        </React.Fragment>
+    )
+     
+ }
